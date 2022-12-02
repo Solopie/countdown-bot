2034b02d9848b89a64f858e9844168d4347038f1
Jesse Nguyen <jesse_nguyen9@yahoo.com>
2022-12-03 01:08:24 +1100

    Fix more than a day hours

--- src/utils/countdown.js
+++ src/utils/countdown.js
@@ -4,17 +4,23 @@ const curTime = () => {
     const eventTime = moment("2022-12-04 13:00+00:00"); // Start of HTB Event
     const currentTime = moment();
     const diffTime = eventTime.valueOf() - currentTime.valueOf();
+
+    if(diffTime < 0) {
+        result = "Event has ended!";
+        return result;
+    }
+
     const duration = moment.duration(diffTime, 'milliseconds');
     let result = "";
     if (duration.hours() >= 1) {
-        result = ('00' + duration.hours()).slice(-2) + " hours";
-    } else if (duration.hours() == 0) {
+        result = ('00' + duration.asHours()).slice(-2) + " hours";
+    } else if (duration.asHours() == 0) {
         // Round down minutes
         result = ('00' + ((duration.minutes() / 10) * 10)).slice(-2) + " minutes";
     } else if (duration.minutes() < 10) {
         result = "Less than 10 minutes remaining!"
     } else {
-        result = "Event has started!"
+        result = "Event has ended!"
     }
     return result;
 }
